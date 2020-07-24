71f49d5f46b64944f8d87e57db4f8741bc851f10
Nimain <nimain@live.com>
2020-07-25 05:05:50 +0530

    update client with ..

---
+++ build/precache-manifest.2c3fca38d5407e7b0c73e52d4f7f72ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016ea0a7d788f535388366a7f90063d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ad37c29b4ab2872f0f8",
+    "url": "/static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "af669dffee62fdc2499e",
+    "url": "/static/css/main.0d08b65d.chunk.css"
+  },
+  {
+    "revision": "1ad37c29b4ab2872f0f8",
+    "url": "/static/js/2.0a0d0d67.chunk.js"
+  },
+  {
+    "revision": "2c26aa7151128ef885feb8bea14f0f60",
+    "url": "/static/js/2.0a0d0d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af669dffee62fdc2499e",
+    "url": "/static/js/main.fa721274.chunk.js"
+  },
+  {
+    "revision": "6c049a7a6721f4ae5b98",
+    "url": "/static/js/runtime-main.5729a589.js"
+  }
+]);
